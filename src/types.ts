b9ff4ee0f309f34ae88e787ff0abf2404920ec16
Patricio Stickar <58763654+patostickar@users.noreply.github.com>
2022-11-13 20:22:40 +0100

    add and extend Entry types

--- src/types.ts
+++ src/types.ts
@@ -2,14 +2,6 @@ export enum Gender {
   Male = 'male',
   Female = 'female',
 }
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {}
-
-export interface DiagnoseEntry {
-  code: string;
-  name: string;
-  latin?: string;
-}
 
 export interface Patient {
   id: string;
@@ -20,6 +12,65 @@ export interface Patient {
   occupation: string;
   entries: Entry[];
 }
+
+export interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<DiagnoseEntry['code']>;
+}
+
+export interface DiagnoseEntry {
+  code: string;
+  name: string;
+  latin?: string;
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: 'Hospital';
+  discharge: Discharge;
+}
+
+interface Discharge {
+  date: string;
+  criteria: string;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: 'OccupationalHealthcare';
+  employerName: string;
+  sickLeave?: { startDate: string; endDate: string };
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: 'HealthCheck';
+  healthCheckRating: HealthCheckRating;
+}
+
+export enum HealthCheckRating {
+  'Healthy' = 0,
+  'LowRisk' = 1,
+  'HighRisk' = 2,
+  'CriticalRisk' = 3,
+}
+
+/*
+
+An important point concerning unions is that, when you use them with Omit to exclude a property,
+it works in a possibly unexpected way. Suppose we want to remove the id from each Entry.
+We could think of using Omit<Entry, 'id'>, but it wouldn't work as we might expect.
+In fact, the resulting type would only contain the common properties, but not the ones they don't share.
+A possible workaround is to define a special Omit-like function to deal with such situations:
+
+// Define special omit for unions
+type UnionOmit<T, K extends string | number | symbol> = T extends unknown ? Omit<T, K> : never;
+// Define Entry without the 'id' property
+type EntryWithoutId = UnionOmit<Entry, 'id'>;
+
+*/
+
+export type Entry = HospitalEntry | OccupationalHealthcareEntry | HealthCheckEntry;
 
 export type WithoutSsnPatient = Omit<Patient, 'ssn'>;
 export type NewPatient = Omit<Patient, 'id'>;
